5d1d65c19fd16d78b69259b105e7fe76726e6332
Kareem <mahmoudelsonbaty777@gmail.com>
2025-01-10 01:58:49 +0200

    add product review task
    add the following for product review model
    1 - model
    2 - GCUProductReviewDTO (get create update)
    3 - repository class and interface
    4 - service class and interface

---
+++ Handmade.Infrastructure/ProductReviewRepository.cs
@@ -0,0 +1,15 @@
+﻿using Handmade.Application.Contracts;
+using Handmade.Context;
+using Handmade.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handmade.Infrastructure
+{
+    public class ProductReviewRepository(HandmadeContext context) : GenericRepository<ProductReview, int>(context), IProductReviewRepository
+    {
+    }
+}
