5d7f26cdd0407ec1b7fb89684f63f46af00950cf
ahmed mohamed <ahmedalghoul10@gmail.com>
2025-04-17 22:18:27 +0200

    PaymentModel
    copy from last version

---
+++ Handmade.Models/PaymentMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handmade.Models
+{
+
+    public class PaymentMethod:BaseEntity<int>
+    {
+        public int MethodId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string MethodEnName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string MethodArName { get; set; }
+
+        [Column(TypeName = "nvarchar(500)")]
+        public string EnDescription { get; set; }
+        public string ArDescription { get; set; }
+
+    }
+}
